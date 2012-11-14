669b0244f85a201ddddf0d49c74d6dc47efcb777
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-11-14 19:28:05 +0400

    add new attachments and remove attachments handlers

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # attach_by_url
 
 Description goes here
+
+## Credits
+
+Thanks to Mark James for silk icons!
+http://www.famfamfam.com/lab/icons/silk/
--- assets/javascripts/redmine_attach_by_url.js
+++ assets/javascripts/redmine_attach_by_url.js
@@ -1,36 +1,40 @@
-//TODO: add handlers to these classes download, delete, add_attachment-by-url
-
 jQuery(document).ready(function($) {
   // move attachments-by-url-fieldset into the right place
   $('#attachments-by-url-fieldset')
     .appendTo($('#attachments_fields').closest('.box'));
 
-  // add check url handler
+  // check url handler
   $('#attachments-by-url')
     .on("keyup change click", ".attachment-by-url input.file-url", function(evt) {
-      setTimeout
       var regUrl = /^(https?:\/\/)([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?[\?]?(\w+=[^&]*&?)*$/
       var isBadUrl =  !(this.value == "" || regUrl.test(this.value));
       $(this).closest('.attachment-by-url').toggleClass('bad-url', isBadUrl);
     });
+
+  // add new attachment handler
+  $(".add-attachment-by-url").on("click", function(evt) {
+    evt.stopPropagation();
+    if ($('.attachment-by-url').length >= 10) return false;
+    fileFieldCount++;
+    var newAttach = $('.attachment-by-url').last().clone();
+    newAttach.find('input.file-url').val('').change();
+    newAttach.find('input.description').val('');
+    newAttach.find('input.file-url').attr("name", "attachments[" + fileFieldCount + "][file-url]");
+    newAttach.find('input.file-by-url').attr("name", "attachments[" + fileFieldCount + "][file]");
+    newAttach.find('input.description').attr("name", "attachments[" + fileFieldCount + "][description]");
+    newAttach.appendTo($('#attachments-by-url'));
+    return false;
+  });
+
+  // remove attachment handler
+  $('#attachments-by-url').on("click", ".button-delete", function(evt) {
+    evt.stopPropagation();
+    if ($('.attachment-by-url').length > 1) {
+      $(this).closest('.attachment-by-url').remove();
+    }
+    return false;
+  });
 });
-
-function addFileByUrlField(blob) {
-  var fields = $('attachments-by-url_fields');
-  if (fields.childElements().length >= 10) return false;
-  fileFieldCount++;
-  var s = new Element('div');
-  s.update(fields.down('div').innerHTML);
-
-  var fileUrl = s.down('input.file-url');
-  fileUrl.name = "attachments[" + fileFieldCount + "][file-url]";
-  fileUrl.value = "";
-  bindHandlerToFileUrlChange(fileUrl);
-
-  s.down('input.file_by_url').name = "attachments[" + fileFieldCount + "][file]";
-  s.down('input.description').name = "attachments[" + fileFieldCount + "][description]";
-  fields.appendChild(s);
-}
 
 function downloadByUrl(el) {
 
