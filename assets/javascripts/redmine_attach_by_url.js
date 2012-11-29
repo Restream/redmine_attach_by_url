eb6b0c5893167c8611c1be7496a306197354b3d1
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-11-29 18:00:01 +0400

    modified form for new downloading process

--- assets/javascripts/redmine_attach_by_url.js
+++ assets/javascripts/redmine_attach_by_url.js
@@ -3,26 +3,40 @@ jQuery(document).ready(function($) {
   $('#attachments-by-url-fieldset')
     .appendTo($('#attachments_fields').closest('.box'));
 
-  // check url handler
-  $('#attachments-by-url')
-    .on("keyup change click", ".attachment-by-url input.file-url", function(evt) {
-      var regUrl = /^(https?:\/\/)([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?[\?]?(\w+=[^&]*&?)*$/
-      var isBadUrl =  !(this.value == "" || regUrl.test(this.value));
-      $(this).closest('.attachment-by-url').toggleClass('bad-url', isBadUrl);
-    });
+  function cleanArray(actual){
+    var newArray = new Array();
+    for(var i = 0; i < actual.length; i++) {
+      if (actual[i]) {
+        newArray.push(actual[i]);
+      }
+    }
+    return newArray;
+  }
+
+  function changeAttachByUrlState(attach, newState) {
+    var newClass = cleanArray([".attachment-by-url.", newState]).join(".");
+    $(attach).css("class", newClass)
+  }
 
   // add new attachment handler
   $("#attachments-by-url-fieldset a.add_attachment").on("click", function(evt) {
     evt.stopPropagation();
     if ($('.attachment-by-url').length >= 10) return false;
     fileFieldCount++;
+
     var newAttach = $('.attachment-by-url').last().clone();
-    newAttach.find('input.file-url').val('').change();
-    newAttach.find('input.description').val('');
+
+    newAttach.find('input.id,input.file-url,input.description').val('');
+
+    newAttach.find('input.id').attr("name",
+      "attachments_by_url[" + fileFieldCount + "][id]");
     newAttach.find('input.file-url').attr("name",
       "attachments_by_url[" + fileFieldCount + "][url]");
     newAttach.find('input.description').attr("name",
       "attachments_by_url[" + fileFieldCount + "][description]");
+
+    changeAttachByUrlState(newAttach, "ready");
+
     newAttach.appendTo($('#attachments-by-url'));
     return false;
   });
@@ -33,6 +47,28 @@ jQuery(document).ready(function($) {
     if ($('.attachment-by-url').length > 1) {
       $(this).closest('.attachment-by-url').remove();
     }
+    return false;
+  });
+
+  // start download attchment
+  $('#attachments-by-url').on("click", ".attachment-by-url .button-attachment-download", function(evt) {
+
+    evt.stopPropagation();
+    changeAttachByUrlState($(this).closest('.attachment-by-url'), "in-progress");
+
+    // TODO: call attchments_by_url#create
+
+    return false;
+  });
+
+  // cancel download attchment
+  $('#attachments-by-url').on("click", ".attachment-by-url.in-progress .button-cancel", function(evt) {
+
+    evt.stopPropagation();
+    changeAttachByUrlState($(this).closest('.attachment-by-url'), null);
+
+    // TODO: call attchments_by_url#cancel
+
     return false;
   });
 });
